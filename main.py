1811a37338a6f2caecf3c1b87cbf4efee3d9828b
Dub1250 <ruarke@oregonstate.edu>
2025-02-02 23:42:37 -0500

    Character Count

--- main.py
+++ main.py
@@ -2,13 +2,23 @@
 # Boot.dev Guided Project: BookBot
 
 def main():
+    charDict = {}
+
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         words = file_contents.split()
-        wordCount = len(words)
+#       wordCount = len(words)
 
-    print(wordCount)
-    
+    for word in words:
+        for i in word:
+            p = i.lower()
+            if p in charDict:
+                charDict[p] += 1
+            else:
+                charDict[p] = 1
+
+    print(charDict)
+
 
 if __name__ == "__main__":
     main()
