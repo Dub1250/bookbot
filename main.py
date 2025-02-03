b091e93e7c150e2b83c9bfb30ae9c889d6ba1265
Dub1250 <ruarke@oregonstate.edu>
2025-02-02 20:16:39 -0500

    Open/Read File function

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+# Author Ethan Ruark
+# Boot.dev Guided Project: BookBot
+
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+
+    print(file_contents)
+
+if __name__ == "__main__":
+    main()
