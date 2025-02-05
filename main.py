874d7111ca2b3143fcf044148b4e808c54a146b6
Dub1250 <ruarke@oregonstate.edu>
2025-02-04 20:13:14 -0500

    Removed hard coded source text, replaced with User Input

--- main.py
+++ main.py
@@ -30,11 +30,13 @@ def wordCount(file):
 
 def main():    
     print("*" * 50)
-    print("Welcome to the new and upgraded BookBot")
+    print("   ", "Welcome to the new and improved BookBot")
     print("*" * 50)
     
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
+    source = input("\nPlease insert the file path of the text you wish to analyze: ")
+    
+    with open(source) as src:
+        file_contents = src.read()
 #    for c in file_contents:
 #        i = c.lower()#        
 #        if i.isalpha():
@@ -45,8 +47,8 @@ def main():
     wordCounts = wordCount(file_contents)
     charDict = characterCount(file_contents)
 
-    print("--- Begin frankenstein.txt letter count ---")
-    print(f"{wordCount} words found in the document\n\n")
+    print("\n--- Begin frankenstein.txt letter count ---\n")
+    print(f"{wordCounts[0]} words found in the document\n")
     for c in charDict:
         print(f"The '{c}' character was found {charDict[c]} times")
     print("\n--- End Report ---\n")
